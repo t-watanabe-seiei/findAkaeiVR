387904da8d08a305215bd14323435ef2594f3af0
t-watanabe <t-watanabe@seiei.ac.jp>
2024-10-08 02:49:55 +0900

    <a-assets timeout="11000"> タイムアウト時間　11秒   パーティクルあり　aframe1.2

--- resources/views/movieTest.blade.php
+++ resources/views/movieTest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>A-Frame Particle System Component Example</title>
     <meta name="description" content="Hello, World!">
-    <script src="https://aframe.io/releases/1.4.2/aframe.min.js"></script> 
+    <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script> 
     <script src="https://cdn.jsdelivr.net/gh/c-frame/aframe-extras@7.2.0/dist/aframe-extras.min.js"></script>
     <!-- <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script> -->
     <!-- <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script> -->
@@ -12,7 +12,7 @@
     
     <!-- <script src="https://unpkg.com/aframe-particle-system-component@1.0.x/dist/aframe-particle-system-component.min.js"></script> -->
     
-    <!-- <script src="{{ asset('js/aframe-particle-system-component.min.js') }}"></script> -->
+    <script src="{{ asset('js/aframe-particle-system-component.min.js') }}"></script>
     <script>
       AFRAME.registerComponent('hit-box', {
         init: function () {
@@ -31,7 +31,7 @@
   </head>
   <body>
     <a-scene>
-      <a-assets timeout="9000"> <!--  タイムアウト時間　9秒  -->
+      <a-assets timeout="6000"> <!--  タイムアウト時間　6秒  -->
           <a-asset-item id="akaeiModel_01" src="{{ asset('cg/akaei_oldMan_idle.glb') }}"></a-asset-item>
           
           <video id="video" src="{{ asset('cg/R0010008_st_001.MP4') }}"
@@ -58,15 +58,15 @@
 
 
       <!-- Particle system uses 'default' preset, setting custom colors. -->
-      <!-- <a-entity position="0 2.25 -15" particle-system="preset:star; color: #f216b0,#f24535"></a-entity> -->
+      <a-entity position="0 2.25 -15" particle-system="preset:star; color: #f216b0,#f24535"></a-entity>
       <!-- <a-entity position="0 2.25 -15" particle-system="preset: dust; particleCount: 10000"></a-entity> -->
       <!-- <a-entity position="0 2.25 -15" particle-system="preset: snow; particleCount: 5000"></a-entity> -->
       <!-- <a-entity position="0 2.25 -15" particle-system="preset: rain"></a-entity> -->
 
-      <a-sky color="#000000"></a-sky>
+      <!-- <a-sky color="#000000"></a-sky> -->
 
       <!-- 実際にVRで表示されるモデル内側に指定リソースを表示する球体オブジェクト 　この球体に３６０videoを投影-->
-      <a-videosphere id="videosphere" src='#video' visible="false"></a-videosphere>
+      <a-videosphere id="videosphere" src='#video'></a-videosphere>
     </a-scene>
   </body>
 </html>
