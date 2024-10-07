887c4cf88a34bda9d3c492824839dbf5f0ebca4c
t-watanabe <t-watanabe@seiei.ac.jp>
2024-10-08 02:50:38 +0900

    <a-assets timeout="11000"> タイムアウト時間11秒   パーティクル無し　aframe1.2++

--- resources/views/movieTest.blade.php
+++ resources/views/movieTest.blade.php
@@ -31,7 +31,7 @@
   </head>
   <body>
     <a-scene>
-      <a-assets timeout="6000"> <!--  タイムアウト時間　6秒  -->
+      <a-assets timeout="11000"> <!--  タイムアウト時間　11秒  -->
           <a-asset-item id="akaeiModel_01" src="{{ asset('cg/akaei_oldMan_idle.glb') }}"></a-asset-item>
           
           <video id="video" src="{{ asset('cg/R0010008_st_001.MP4') }}"
