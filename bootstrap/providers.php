3b0335eed89b9d7a939b6a158f9bbe1a564b8567
DESKTOP-AI93RB8\t-watanabe <t-watanabe@seiei.ac.jp>
2025-10-21 08:37:21 +0900

    shooting3Dmodel 33 +5

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
+    // App\Providers\RouteServiceProvider::class, // Temporarily disabled
 ];
