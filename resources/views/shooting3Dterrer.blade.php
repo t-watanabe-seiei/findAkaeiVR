0e48748161861fab7b22304b8a9667e5589a599e
DESKTOP-AI93RB8\t-watanabe <t-watanabe@seiei.ac.jp>
2025-11-06 17:26:56 +0900

    zombie oda fix

--- resources/views/shooting3Dterrer.blade.php
+++ resources/views/shooting3Dterrer.blade.php
@@ -2558,7 +2558,7 @@
             <a-asset-item id="model_02" src="{{ asset('cg/zombie_fujii.glb') }}"></a-asset-item>
             <a-asset-item id="model_03" src={{ asset('cg/zombie_isobe.glb') }}></a-asset-item>
             <a-asset-item id="model_04" src={{ asset('cg/zombie_ootani.glb') }}></a-asset-item>
-            <a-asset-item id="model_05" src={{ asset('cg/zombie_oda.glb') }}></a-asset-item>
+            <a-asset-item id="model_05" src={{ asset('cg/zombie_oda2.glb') }}></a-asset-item>
             <a-asset-item id="model_06" src={{ asset('cg/zombie_ishimaru.glb') }}></a-asset-item>
 
             <!-- サウンド -->
