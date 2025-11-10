e53fa67dc8d076d28752769e884faa807a7d564a
DESKTOP-AI93RB8\t-watanabe <t-watanabe@seiei.ac.jp>
2025-11-11 06:28:59 +0900

    AR 025 drag&pinchInOut 04

--- resources/views/ARstampRally.blade.php
+++ resources/views/ARstampRally.blade.php
@@ -375,8 +375,8 @@
             </a-entity>
             
             <!-- ライトを追加して明るくする -->
-            <a-light type="ambient" intensity="1.3"></a-light>
-            <a-light type="directional" intensity="0.7" position="1 1 1"></a-light>
+            <a-light type="ambient" intensity="1.56"></a-light>
+            <a-light type="directional" intensity="0.84" position="1 1 1"></a-light>
         </a-marker>
         
     </a-scene>
