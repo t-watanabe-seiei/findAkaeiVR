a64b533bfa39c99377cc0d4cca624dfd150d6131
DESKTOP-AI93RB8\t-watanabe <t-watanabe@seiei.ac.jp>
2026-04-17 18:49:46 +0900

    ARstampRally202605 鯉のぼり 08

--- resources/views/ARstampRally202605/scene.blade.php
+++ resources/views/ARstampRally202605/scene.blade.php
@@ -29,7 +29,7 @@
         smoothCount="10"
         smoothTolerance="0.01"
         smoothThreshold="5">
-        <a-cylinder color="#ffffff" height="4" radius="0.05" position="0.6 1.5 0"></a-cylinder>
+        <a-cylinder color="#ffffff" height="4" radius="0.05" position="0.65 1.5 0"></a-cylinder>
          <a-sphere color="#ffea00" radius="0.1" position="0.6 3.5 0"></a-sphere>
     </a-marker>
 
