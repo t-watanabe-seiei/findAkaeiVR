b2b9eb02dd42fee572c127a156906b3b79611c84
DESKTOP-AI93RB8\t-watanabe <t-watanabe@seiei.ac.jp>
2026-04-18 08:28:55 +0900

    ARstampRally202604 22 ぶっちーfix3

--- resources/views/ARstampRally202605/scene.blade.php
+++ resources/views/ARstampRally202605/scene.blade.php
@@ -38,8 +38,8 @@
         <a-entity
             id="model-00"
             gltf-model="{{ asset('cg/202605/Model_00.glb') }}"
-            position="1 0 0.5"
-            scale="0.5 0.5 0.5"
+            position="1.1 0 0.5"
+            scale="0.6 0.6 0.6"
             rotation="0 0 0"
             visible="false">
         </a-entity>
